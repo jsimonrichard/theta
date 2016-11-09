eaaa650c040fbbbdeef0bcfd3cd5e22e5c7915e5
jsimonrichard <jsimonrichard@gmail.com>
2016-11-09 15:43:46 -0500

    Added decode

--- theta.py
+++ theta.py
@@ -1,5 +1,6 @@
 import re
 import random
+import math
 
 letters = ['a', 'b', 'c', 'd', 'e',
            'f', 'g', 'h', 'i', 'j',
@@ -14,43 +15,95 @@ def clean_string(string):
 def shift (letter, shift):
     index = int(letters.index(letter))
     return letters[ (index + int(shift)) % 26]
-    
-def shift_key(string, key):
-    output = []
-    key = list(str(key))
-    string = clean_string(string)
-    
-    for i in range(len(string)):      
-        index = int(letters.index( string[i] ))
-        shift_amount = key[i % len(key)]
-        output.append(shift( string[i], shift_amount ))
-    return ''.join(output)
 
-def sort_key(string, key):
-    output = []
-    temp = []
-    key = list(str(key))
-    string = clean_string(string)
-
-    #assign
-    for i in range(len(string)):      
-        temp.append([string[i], key[i % len(key)]])
-
-    #sort
-    for n in range(0, 9):
-        for s in temp:
-            if int(s[1]) == int(n):
-                output.append(s[0])
-
+class shift_key(object):
+    def encode(string, key):
+        output = []
+        key = list(str(key))
+        string = clean_string(string)
+        
+        for i in range(len(string)):      
+            index = int(letters.index( string[i] ))
+            shift_amount = key[i % len(key)]
+            output.append(shift( string[i], shift_amount ))
+        return ''.join(output)
     
-    
-    return ''.join(output)
+    def decode(string, key):
+        output = []
+        key = list(str(key))
+        string = clean_string(string)
+        
+        for i in range(len(string)):      
+            index = int(letters.index( string[i] ))
+            shift_amount = -int((key[i % len(key)]))
+            output.append(shift( string[i], shift_amount ))
+        return ''.join(output)
+
+
+class sort_key(object):
+    def encode(string, key):
+        output = []
+        temp = []
+        key = list(str(key))
+        string = clean_string(string)
+
+        #assign
+        for i in range(len(string)):      
+            temp.append([string[i], key[i % len(key)]])
+
+        #sort
+        for n in range(0, 9):
+            for s in temp:
+                if int(s[1]) == int(n):
+                    output.append(s[0])
+
+        return ''.join(output)
+
+    def decode(string, key):
+        output = []
+        temp = []
+        key = list(str(key))
+        string = clean_string(string)
+
+        comp_list = []
+        decode_key = []
+
+        for i in range(math.ceil(len(string) / len(key))):
+            comp_list += key
+
+        for i in range(len(comp_list) - len(string)):
+            del comp_list[len(comp_list) - 1]
+
+        for n in range(0, 9):
+            count = comp_list.count(str(n))
+            for i in range(count):
+                decode_key.append(str(n))
+            
+        #assign
+        for i in range(len(string)):      
+            temp.append([string[i], decode_key[i]])
+
+        #sort
+        for i in comp_list:
+            for s in range(len(temp)):
+                if int(temp[s][1]) == int(i):
+                    output.append(temp[s][0])
+                    del temp[s]
+                    break
+        
+        return ''.join(output)
 
 def str_to_int(string):
     string = clean_string(string)
     output = []
     for i in string:
         output.append( int(letters.index(i)) + 1 )
+    return output
+
+def int_to_string(int_list):
+    output = []
+    for i in int_list:
+        output.append( letters[i - 1] )
     return output
 
 def onetime_pad(length):
